d4f33a347750ddad22c33f179a05ccd500d6d91e
IshaanTakrani <ishaantakrani@gmail.com>
2025-08-08 15:38:38 -0400

    ahhhhhh

--- raisearch/src/components/HorizontalNav.tsx
+++ raisearch/src/components/HorizontalNav.tsx
@@ -3,14 +3,20 @@ import { Button } from './ui/button';
 
 function HorizontalNav() {
 	return (
-		<div className="fixed top-0 right-0 flex gap-6 p-3 px-10 m-0 w-full justify-end text-[var(--foreground)] border-b">
-			<button className="p-2 px-5 rounded-lg transition duration-100 ease-in-out hover:cursor-pointer border-1 hover:border-[var(--signature-orange)]">
-				Dashboard
-			</button>
-			<button className="p-2 rounded-lg">My Papers</button>
-			<button className="p-2 rounded-lg bg-[var(--signature-orange)] text-[var(--background)]">
-				Make a Paper
-			</button>
+		<div className="fixed top-0 right-0 flex justify-between items-center w-full gap-6 p-4 pr-10 pl-6 m-0 text-[var(--foreground)] border-b">
+			<img src="logo.png" className="h-10" alt="Logo" />
+
+			<div className="flex gap-4">
+				<button className="p-2 px-5 rounded-lg transition duration-100 ease-in-out hover:cursor-pointer border hover:border-[var(--primary)]">
+					Dashboard
+				</button>
+				<button className="p-2 px-5 rounded-lg transition duration-100 ease-in-out hover:cursor-pointer border hover:border-[var(--primary)]">
+					My Papers
+				</button>
+				<button className="p-2 px-5 rounded-lg transition duration-100 ease-in-out border border-solid border-[var(--primary)] hover:cursor-pointer bg-[var(--primary)] text-[var(--background)] hover:bg-[var(--background)] hover:text-[var(--primary)]">
+					Make a Paper
+				</button>
+			</div>
 		</div>
 	);
 }
